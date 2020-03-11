82ee35189ef571bd863cdda36c05326ded1e3b60
CEDRIC MATASO <langemataso@gmail.com>
2020-03-11 13:42:55 +0200

    added jquery ui

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,6 +20,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link
 		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 		rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/jquery-ui.min.css');?>" type="text/css">
+	
+
+	
+
 
 	<!-- Custom styles for this template-->
 	<link href="<?php echo base_url('assets/css/sb-admin-2.min.css');?>" rel="stylesheet">
@@ -33,14 +38,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 			<!-- Sidebar - Brand -->
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url('dashboard/index')?>">
+			<a class="sidebar-brand d-flex align-items-center justify-content-center"
+				href="<?php echo base_url('dashboard/index')?>">
 				<div class="sidebar-brand-icon rotate-n-15">
 					<i class="fas fa-laugh-wink"></i>
 				</div>
 				<div class="sidebar-brand-text mx-3"><?php if (!empty($header['app_name'])) {echo $header['app_name'];}else {
 				 	 echo "SADROM <sup>green</sup>";
 				}?>
-				</div>							
+				</div>
 			</a>
 
 			<!-- Divider -->
@@ -91,7 +97,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					data-parent="#accordionSidebar">
 					<div class="bg-white py-2 collapse-inner rounded">
 						<h6 class="collapse-header">Message Menu</h6>
-						<a class="collapse-item" href="utilities-color.html">Envoyer SMS</a>
+						<a class="collapse-item" href="<?php echo base_url('messaging/sms');?>">Envoyer SMS</a>
 						<a class="collapse-item" href="<?php echo base_url('messaging/template');?>">SMS Template</a>
 					</div>
 				</div>
@@ -102,7 +108,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 			<!-- Heading -->
 			<div class="sidebar-heading">
-				Addons
+				Administration
 			</div>
 
 			<!-- Nav Item - Pages Collapse Menu -->
@@ -114,25 +120,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</a>
 				<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
 					<div class="bg-white py-2 collapse-inner rounded">
-						<h6 class="collapse-header">Login Screens:</h6>
+
 						<a class="collapse-item" href="<?php echo base_url('settings/apis');?>">APIs </a>
 						<a class="collapse-item" href="<?php echo base_url('settings/get_app_setting')?>">General</a>
-						<a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-						<div class="collapse-divider"></div>
-						<h6 class="collapse-header">Other Pages:</h6>
-						<a class="collapse-item" href="404.html">404 Page</a>
-						<a class="collapse-item" href="blank.html">Blank Page</a>
 					</div>
 				</div>
 			</li>
 
 			<!-- Divider -->
 			<hr class="sidebar-divider d-none d-md-block">
-				<p class="align-center">
-				  Version <?php if ($header['app_version'] !=null) {
-					  echo $header['app_version'];
-				  }?>
-				</p>
+
 			<!-- Sidebar Toggler (Sidebar) -->
 			<div class="text-center d-none d-md-inline">
 				<button class="rounded-circle border-0" id="sidebarToggle"></button>
